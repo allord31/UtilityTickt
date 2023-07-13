ffa2b1dece7aa3fa550fb88252eac18715871b1f
Ahmad Alwardy <ahmad.alwardy@openknowledge.de>
2023-07-13 17:18:52 +0200

    version 1.1

---
+++ src/main/java/de/openknowledge/Main.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) open knowledge GmbH.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.openknowledge;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import domain.Wurfel;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> wuerfel = new Wurfel().getWuerfel();
+        int randomWurf = new Random().nextInt(1, 7);
+        System.out.println(wuerfel.get(randomWurf));
+    }
+}
---
+++ src/main/java/domain/Wurfel.java
@@ -0,0 +1,33 @@
+package domain;/*
+ * Copyright (C) open knowledge GmbH.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.util.ArrayList;
+
+public class Wurfel {
+    private final ArrayList<String> wuerfel = new ArrayList<>();
+    public Wurfel(){
+        wuerfel.add("Würfel Seite Eins: 1");
+        wuerfel.add("Würfel Seite Zwei: 2");
+        wuerfel.add("Würfel Seite Drei: 3");
+        wuerfel.add("Würfel Seite Vier: 4");
+        wuerfel.add("Würfel Seite Fünf: 5");
+        wuerfel.add("Würfel Seite Sechs: 6");
+    }
+    public ArrayList<String> getWuerfel() {
+        return wuerfel;
+    }
+
+}
