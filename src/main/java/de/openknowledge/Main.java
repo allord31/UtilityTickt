e07b4244e71a00a8ca3963c42ef4f597217a58f7
Ahmad Alwardy <ahmad.alwardy@openknowledge.de>
2023-07-14 09:43:54 +0200

    Version 1.2 Deleting 2 print Syntax now ther is not print Syntax

--- src/main/java/de/openknowledge/Main.java
+++ src/main/java/de/openknowledge/Main.java
@@ -28,8 +28,5 @@ public class Main {
         System.out.println(wuerfel.get(randomWurf));
         randomWurf = new Random().nextInt(1,7);
         System.out.println(new ZweiteWuerfel().getZweitewuerfel().get(randomWurf)) ;
-        System.out.println("some Changes 1");
-        System.out.println("replacing hier");
-
     }
 }
