0d33270a49271399ddb9c22d307dfd5878d9afcc
Ahmad Alwardy <ahmad.alwardy@openknowledge.de>
2023-07-18 15:46:34 +0200

    Internationalisierung

--- src/main/java/de/openknowledge/Main.java
+++ src/main/java/de/openknowledge/Main.java
@@ -15,26 +15,30 @@
  */
 package de.openknowledge;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Locale localeztion = new Locale("en","US");
+        ResourceBundle langugeResources =  ResourceBundle.getBundle("lang",localeztion);
         Scanner input = new Scanner(System.in);
-        System.out.print("geben Sie die Seite lang von \"A\": ");
+        System.out.print(langugeResources.getString("seiteLangA")+ ": ");
         double a = input.nextDouble();
-        System.out.print("geben Sie die Seite lang von \"B\": ");
+        System.out.print(langugeResources.getString("seiteLangB") + ": ");
         double b = input.nextDouble();
-        System.out.print("geben Sie die Seite lang von \"C\": ");
+        System.out.print(langugeResources.getString("seiteLangC") + ": ");
         double c = input.nextDouble();
         double cosinusAlpha = ((b*b) + (c*c) - (a*a)) /(2 * b * c);
         double cosinusBeta = ((a*a) + (c*c) - (b*b)) / (2 * a * c);
         double cosinusGama = ((a*a) + (b*b) - (c*c)) / (2 * a * b);
         System.out.println(cosinusAlpha + " ;" + cosinusBeta + " ;" + cosinusGama);
-        System.out.println("Winkel Alpha="
+        System.out.println(langugeResources.getString("WinkelAlpha") + "="
             + Math.toDegrees(Math.acos(cosinusAlpha)));
-        System.out.println("Winkel Beta="
+        System.out.println(langugeResources.getString("WinkelBeta") + "="
             + Math.toDegrees(Math.acos(cosinusBeta)));
-        System.out.println("Winkel Gama="
+        System.out.println(langugeResources.getString("WinkelGama") + "="
             + Math.toDegrees(Math.acos(cosinusGama)));
     }
 }
